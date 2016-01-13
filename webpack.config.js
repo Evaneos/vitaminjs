9cf750810ec9c77a80e30b8b7b1605b5e5b97f8f
Lucas <lucas@evaneos.com>
2016-01-13 17:04:35 +0100

    hot module reload

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
 
     // If compilation gets slow, change strategy for prod & dev
-    devtool: 'source-map',
+    // devtool: 'source-map',
 
     module: {
         loaders: [
