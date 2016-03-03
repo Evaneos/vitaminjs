62d22f30d8c63f15999c9535334d9a5649ee0fa1
Johan Girod <johangirod@gmail.com>
2016-03-03 13:37:44 +0100

    Fix dependancies issues

--- src/build_config/webpack.config.server.js
+++ src/build_config/webpack.config.server.js
@@ -3,8 +3,6 @@ import { fondationResolve, appResolve, concat } from '../utils/index';
 import { BannerPlugin } from 'webpack';
 import mergeWith from 'lodash.mergewith';
 import fs from 'fs';
-import path from 'path';
-
 
 const externalModules = (modulesPath) => fs
     .readdirSync(modulesPath)
@@ -12,8 +10,9 @@ const externalModules = (modulesPath) => fs
 const appModules = externalModules(appResolve('node_modules'));
 const fondationModules = externalModules(fondationResolve('node_modules'));
 const whiteList = ['webpack/hot/poll.js?1000'];
-const externals = function (context, request, callback) {
-    var pathStart = request.split('/')[0];
+
+function externals(context, request, callback) {
+    const pathStart = request.split('/')[0];
     if (whiteList.indexOf(request) !== -1) {
         return callback();
     }
@@ -24,13 +23,13 @@ const externals = function (context, request, callback) {
         return callback(null, `commonjs2 fondation/node_modules/${request}`);
     }
     return callback();
-};
+}
 
-module.exports = function (options) {
+module.exports = function serverConfig(options) {
     return mergeWith({}, config(options), {
         entry: [
             ...(options.hot ? ['webpack/hot/poll.js?1000'] : []),
-            fondationResolve('src', 'server', 'index.js'),
+            fondationResolve('src', 'server', 'server.js'),
         ],
         output: {
             path: appResolve('build'),
@@ -54,9 +53,9 @@ module.exports = function (options) {
         },
         plugins: [
             ...(options.dev ? [new BannerPlugin({
-                banner: 'require("source-map-support").install();',
+                banner: 'require("fondation/node_modules/source-map-support").install();',
                 raw: true, entryOnly: false,
-            })] : [])
+            })] : []),
         ],
     }, concat);
 };
