992dbcff4101252f576d5bc42ebdffa98a32c0bb
Johan Girod <johangirod@gmail.com>
2016-03-09 19:36:44 +0100

    Add etags to server

--- src/server/app.js
+++ src/server/app.js
@@ -1,18 +1,21 @@
 import compose from 'koa-compose';
 import serve from 'koa-static';
 import mount from 'koa-mount';
+import etag from 'koa-etag';
+import conditional from 'koa-conditional-get';
+
 import { appResolve } from '../utils';
 import serverConfig from '../app_descriptor/server';
 import renderer from './middleware/renderer';
 import storeCreator from './middleware/store';
 import router from './middleware/router';
 
-
 export default compose([
+    conditional(),
+    etag(),
     mount(serverConfig.publicUrl, serve(appResolve(serverConfig.publicPath))),
     ...(serverConfig.middlewares || []),
     router(),
     storeCreator(),
     renderer(),
 ]);
-
