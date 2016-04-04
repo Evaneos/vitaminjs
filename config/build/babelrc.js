957abe64a497d22360f3e2d89d37e4e1152f738f
Johan Girod <johangirod@gmail.com>
2016-04-04 18:46:45 +0200

    Fix babelrc config : can\'t use require.resolve when the babelrc is required from the server
    Canveat : npm link might not work anymore with this package

--- config/build/babelrc.js
+++ config/build/babelrc.js
@@ -2,15 +2,15 @@ import { vitaminResolve } from '../utils';
 export default (env) => ({
     presets: [
         ...(env === 'client' ?
-            [require.resolve('babel-preset-es2015-webpack')] :
-            [require.resolve('babel-preset-es2015-node5')]
+            ['es2015-webpack'] :
+            ['es2015-node5']
         ),
-        require.resolve('babel-preset-react'),
-        require.resolve('babel-preset-stage-1'),
+        'react',
+        'stage-1',
     ],
     plugins: [
         // Make optional the explicit import of React in JSX files
-        require.resolve('babel-plugin-react-require'),
+        'react-require',
     ],
     sourceRoot: vitaminResolve(),
 });
