7b0b045816ac55577cc69306c4dccb2aba7bf267
Johan Girod <johangirod@gmail.com>
2016-09-05 13:52:46 +0200

    Update dependancies

--- config/build/babelrc.js
+++ config/build/babelrc.js
@@ -1,12 +1,11 @@
 import { vitaminResolve } from '../utils';
 export default (env) => ({
     presets: [
-        ...(env === 'client' ?
-            ['es2015'] :
-            ['es2015-node5']
+        (env === 'client' ?
+            ['es2015', { modules: false }] :
+            'es2015-node5'
         ),
-        'react',
-        'stage-1',
+        'react', 'stage-1',
     ],
     plugins: [
         // Make optional the explicit import of React in JSX files
