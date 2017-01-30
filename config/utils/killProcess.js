063df60777ff7258af8ff0717cd19bba1ccbf5b2
Christophe Hurpeau <christophe@evaneos.com>
2017-01-30 12:17:26 +0100

    missing file killProcess

---
+++ config/utils/killProcess.js
@@ -0,0 +1,22 @@
+const ALLOWED_SIGNALS = ['SIGTERM', 'SIGINT', 'SIGQUIT'];
+
+export default (process, { signal = 'SIGTERM', timeout = 4000 } = {}) => {
+    if (!ALLOWED_SIGNALS.includes(signal)) {
+        throw new Error(`Invalid signal: "${signal}", expecting one of ${ALLOWED_SIGNALS.join()}`);
+    }
+
+    return new Promise((resolve) => {
+        const killTimeout = setTimeout(() => {
+            // eslint-disable-next-line no-console
+            console.warn('Timeout: sending SIGKILL...');
+            process.kill('SIGKILL');
+        }, timeout);
+
+        process.removeAllListeners();
+        process.addListener('exit', () => {
+            clearTimeout(killTimeout);
+            resolve();
+        });
+        process.kill(signal);
+    });
+};
