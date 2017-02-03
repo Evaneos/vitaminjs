5c1fc5ab240829f67576b3fa792bc0d613000e96
Christophe Hurpeau <christophe.hurpeau+github@gmail.com>
2017-02-03 17:42:14 +0100

    feat: upgrade babel dependencies (#211)
    * feat: upgrade babel dependencies
    
    * feat: remove babel-preset-es2015-node6
    
    * docs: babelrc add plugins doc
    
    * fix: babel preset node6 required for bin

--- config/build/babelrc.js
+++ config/build/babelrc.js
@@ -1,9 +1,9 @@
-import { buildPreset as buildPreset2015 } from 'babel-preset-es2015';
-import presetNode6 from 'babel-preset-es2015-node6/object-rest';
+import { buildPreset as preset2015 } from 'babel-preset-es2015';
 import presetReact from 'babel-preset-react';
 import preset2016 from 'babel-preset-es2016';
 import preset2017 from 'babel-preset-es2017';
 import presetStage1 from 'babel-preset-stage-1';
+import pluginNode6FunctionName from 'babel-plugin-transform-es2015-function-name';
 import pluginReactRequire from 'babel-plugin-react-require';
 import pluginTransformRuntime from 'babel-plugin-transform-runtime';
 import pluginTransformExportDefaultName from 'babel-plugin-transform-export-default-name-forked';
@@ -16,23 +16,24 @@ import { vitaminResolve } from '../utils';
 
 export default env => ({
     presets: [
-        env === 'client' ? [buildPreset2015, { modules: false }] : presetNode6,
+        env === 'client' && [preset2015, { modules: false }],
         presetReact,
         preset2016,
         preset2017,
         presetStage1,
-    ],
+    ].filter(Boolean),
     plugins: [
+        // The only missing plugin for node 6
+        env === 'server' && pluginNode6FunctionName,
         // Make optional the explicit import of React in JSX files
         pluginReactRequire,
         // Add Object.entries, Object.values and other ES2017 functionalities
-        ...(env === 'server' ?
-            [pluginTransformRuntime] :
-            // in the client, we prefer solution like https://polyfill.io/v2/docs/, to keep the
-            // bundle size the smallest possible.
-            []
-        ),
-        // Remove server-only or client-only imports
+        // in the client, we prefer solution like https://polyfill.io/v2/docs/, to keep the
+        // bundle size the smallest possible.
+        env === 'server' && pluginTransformRuntime,
+        // replace process.env.NODE_ENV by its current value
+        pluginNodeEnvInline,
+        // replace IS_CLIENT and IS_SERVER
         [pluginMinifyReplace, {
             replacements: [
                 {
@@ -45,11 +46,14 @@ export default env => ({
                 },
             ],
         }],
-        pluginNodeEnvInline,
+        // Dead code elimination (for example: if (IS_CLIENT) { ... } becames if (false) { }
         [pluginMinifyDeadCodeElimination, { keepFnName: true }],
+        // transforms `IS_CLIENT && doSomething()` => `false && doSomething()` to `false`
         pluginMinifyGuardedExpressions,
-        pluginTransformExportDefaultName,
+        // Remove server-only or client-only imports
         pluginDiscardModuleReferences,
-    ],
+        // easier debugging on export default arrow functions with the filename
+        pluginTransformExportDefaultName,
+    ].filter(Boolean),
     sourceRoot: vitaminResolve(),
 });
