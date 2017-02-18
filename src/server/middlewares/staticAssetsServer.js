c23768f4fc0a1b453f71c69c57e4b156a4c783d7
Christophe Hurpeau <christophe@evaneos.com>
2017-02-18 16:16:12 +0100

    fix: mountPath when basePath is ends with /

--- src/server/middlewares/staticAssetsServer.js
+++ src/server/middlewares/staticAssetsServer.js
@@ -3,8 +3,14 @@ import serve from 'koa-static';
 import mount from 'koa-mount';
 import config from '../../../config';
 
+// examples
+// basePath: / or /app
+// publicPath: / or [/app]/public-path or http://localhost:3000[/app]/public-path
+// parsedPath: / or [/app]/public-path or [/app]/public-path
+// mountPath : / or /public-path
+
 const parsedPath = parseUrl(config.publicPath).pathname || '';
-const mountPath = parsedPath.slice(config.basePath.length);
+const mountPath = parsedPath.substr(config.basePath.replace(/\/+$/, '').length);
 
 const servePublic = serve(config.client.buildPath);
 
