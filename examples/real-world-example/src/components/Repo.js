ce46e8a7d0b8027fea1648d81eda808ea1d53060
Johan Girod <johangirod@gmail.com>
2017-02-20 00:45:43 +0100

    feat: add real-world example from redux repository

---
+++ examples/real-world-example/src/components/Repo.js
@@ -0,0 +1,36 @@
+import { PropTypes } from 'react';
+import { Link } from 'vitaminjs/react-router';
+
+const Repo = ({ repo, owner }) => {
+    const { login } = owner;
+    const { name, description } = repo;
+
+    return (
+    <div className="Repo">
+      <h3>
+        <Link to={`/${login}/${name}`}>
+          {name}
+        </Link>
+        {' by '}
+        <Link to={`/${login}`}>
+          {login}
+        </Link>
+      </h3>
+      {description &&
+        <p>{description}</p>
+      }
+    </div>
+    );
+};
+
+Repo.propTypes = {
+    repo: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string,
+    }).isRequired,
+    owner: PropTypes.shape({
+        login: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default Repo;
