36bec40d930e17725587a576a949dd799c0f0b6b
Jalil Dabbech <jalildabbech@MacBook-Pro-de-Jalil.local>
2017-05-10 10:15:23 +0200

    Add redux persist. Blacklist routing state.

--- src/shared/store.js
+++ src/shared/store.js
@@ -5,6 +5,8 @@ import {
     applyMiddleware,
 } from 'redux';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
+import {persistStore, autoRehydrate} from 'redux-persist'
+
 import thunk from 'redux-thunk';
 /* eslint-disable import/no-extraneous-dependencies */
 import appEnhancers from '__app_modules__redux_enhancers__';
@@ -17,12 +19,13 @@ export function createRootReducer(reducers) {
 export function create(history, reducers, middlewares, initialState) {
     const createStoreWithMiddleware = compose(
         applyMiddleware(...middlewares, thunk, routerMiddleware(history)),
+        autoRehydrate(),
         ...devEnhancers,
         ...appEnhancers,
     )(createStore);
 
     const rootReducer = createRootReducer(reducers);
     const store = createStoreWithMiddleware(rootReducer, initialState);
-
+    persistStore(store, {blacklist: ['routing']});
     return store;
 }
