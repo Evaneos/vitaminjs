7c015739d8bf16cabda823f2036547779e2c6534
Antoine Lépée <alepee@gmail.com>
2017-05-12 16:22:17 +0200

    feat: logger util abstract console methods and format messages properly (#361)
    * feat: logger util abstract console methods and format messages properly
    
    * fix(logger): add blankLines method, remove .replace method and cleanup
    
    * fix(logger): prevent stripNewline to test null and undefined

---
+++ packages/vitaminjs/config/utils/logger.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+
+import chalk from 'chalk';
+
+const stripNewline = str => (
+    (![null, undefined].includes(str) && str.constructor === String)
+        ? str.replace(/^\n+/, '')
+        : str
+);
+
+const blankLines = (times = 1) => (new Array(times))
+    .fill('\n')
+    .join('');
+
+const LOGGERS = [
+    { method: 'success', level: 'log', style: str => chalk.green(`\n${chalk.inverse(' SUCCESS ')} ${stripNewline(str)}`) },
+    { method: 'error', level: 'error', style: str => chalk.red(`\n${chalk.inverse(' ERROR ')} ${stripNewline(str)}`) },
+    { method: 'warning', level: 'warn', style: str => chalk.yellow(`\n${chalk.inverse(' WARNING ')} ${stripNewline(str)}`) },
+    { method: 'info', level: 'info', style: chalk.blue },
+    { method: 'log', level: 'log' },
+    { method: 'blankLines', level: 'log', style: blankLines },
+];
+
+const buildLogger = ({ level, style = str => str }) => (...args) => {
+    console[level](args
+        .map(arg => style(arg))
+        .join('\n'),
+    );
+};
+
+export default LOGGERS.reduce((acc, logger) => (
+    { ...acc, [logger.method]: buildLogger(logger) }
+), {});
