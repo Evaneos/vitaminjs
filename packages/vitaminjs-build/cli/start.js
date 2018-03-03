4c495729a47375f4aedb5b12adc2d72940ddc1f7
Antoine Lépée <alepee@gmail.com>
2018-03-03 12:28:35 +0100

    feat(build): use interfaces build and start server

--- packages/vitaminjs-build/cli/start.js
+++ packages/vitaminjs-build/cli/start.js
@@ -1,62 +1,13 @@
-/* eslint-disable no-console */
-const killProcess = require('../config/utils/killProcess');
-const build = require('./build');
-const checkHot = require('./checkHot');
-const { BUILD_FAILED } = require('./commonBuild');
+const { BUILD_FAILED } = require('./compile');
+const { buildAndWatch, buildAndExit } = require('./build');
+const server = require('./server');
 const clean = require('./clean');
-const listenExitSignal = require('./listenExitSignal');
-const serve = require('./serve');
 
-const start = (options) => {
-    let serverProcess;
+function start(hmr) {
+    const hot = hmr && process.env.NODE_ENV !== 'production';
+    (hot ? buildAndWatch : buildAndExit)({ hot }, server.reload).then(() =>
+        server.start()
+    );
+}
 
-    let exiting = false;
-    listenExitSignal((signal) => {
-        exiting = true;
-        if (!serverProcess) process.exit();
-        killProcess(serverProcess, { signal }).then(() => {
-            console.log('exiting'); process.exit();
-        });
-    });
-
-    function startServer(config) {
-        if (exiting) return;
-        if (serverProcess) {
-            killProcess(serverProcess).then(() => startServer(config));
-            serverProcess = null;
-            return;
-        }
-        serverProcess = serve(config);
-        serverProcess.on('message', (message) => {
-            if (message === 'restart') {
-                // eslint-disable-next-line no-use-before-define
-                startServer(config);
-            }
-        });
-        serverProcess.on('close', () => {
-            serverProcess = null;
-        });
-    }
-
-    const sendSignal = (config) => {
-        if (!serverProcess) {
-            startServer(config);
-            return;
-        }
-
-        serverProcess.kill('SIGUSR2');
-    };
-
-    return build(options, sendSignal, startServer);
-};
-
-module.exports = (hmr) => {
-    clean()
-        .then(() => start({ hot: checkHot(hmr) }))
-        .catch((err) => {
-            if (err !== BUILD_FAILED) {
-                console.log(err.stack || err);
-            }
-            process.exit(1);
-        });
-};
+module.exports = hmr => clean().then(() => start(hmr));
