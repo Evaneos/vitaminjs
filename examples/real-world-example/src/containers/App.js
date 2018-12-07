dbd0cf91cbf36326fa4733418b5d7dbf43d6c5b7
Lucas <lucas@evaneos.com>
2018-12-07 16:03:43 +0100

    chore(replace React.PropTypes in examples):

--- examples/real-world-example/src/containers/App.js
+++ examples/real-world-example/src/containers/App.js
@@ -1,4 +1,5 @@
-import { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
 import { connect } from 'vitaminjs/react-redux';
 import { compose } from 'vitaminjs/redux';
 import { browserHistory } from 'vitaminjs/react-router';
